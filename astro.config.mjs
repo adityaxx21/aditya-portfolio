f63dfdb7d8a6cc4b1f98f526929448ab33f8b9ee
Mohammad Aditya Noviansyah <adityayatma@gmail.com>
2024-06-06 15:50:50 +0700

    fixing #3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-  },
-  output: "hybrid",
-});
+    server: {
+      host: '0.0.0.0', // Specify your host here (e.g., 'localhost' or '0.0.0.0')
+    }
+  });
