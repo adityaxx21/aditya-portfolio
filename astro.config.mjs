72469ed8ed4575fc4ef5b4a99c5acfd67582c61f
Mohammad Aditya Noviansyah <adityayatma@gmail.com>
2024-06-06 14:47:17 +0700

    update host

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+      host: '0.0.0.0', // Specify your host here (e.g., 'localhost' or '0.0.0.0')
+      port: 3000       // Specify your port here
+    }
+  });
